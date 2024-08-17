ff1a0f24150e42f65fef5c2cd348c5798c70568c
osm3rr <osm3rr@adakademy.com>
2024-08-17 11:45:45 -0400

    create

--- post/urls.py
+++ post/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomePageView,DetailPageView
+from .views import HomePageView,DetailPageView,CreatePageView
 
 urlpatterns=[
 
     path("",HomePageView.as_view(),name="home"),
-    path("post/detail/<int:pk>",DetailPageView.as_view(),name="detail")
+    path("post/detail/<int:pk>",DetailPageView.as_view(),name="detail"),
+    path("post/create",CreatePageView.as_view(),name="create")
 ]
--- post/views.py
+++ post/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView
+from django.views.generic import *
+from django.views.generic.edit import CreateView
 from .models import Post
 # Create your views here.
 
@@ -14,6 +15,9 @@ class DetailPageView(DetailView):
     model=Post
     template_name= "detail.html"
 
+class CreatePageView(CreateView):
 
+    model=Post
+    template_name="create.html"
 
-
+    fields=["titulo","descripcion","autor"]
